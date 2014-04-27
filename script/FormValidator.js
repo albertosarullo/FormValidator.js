62f6445d1575ab36681716c3f89c23a47f996d12
Alberto Sarullo <alberto.sarullo@gmail.com>
2014-04-27 17:35:25 +0200

    Rewrite to accept asynchronous function as validation rule

--- script/FormValidator.js
+++ script/FormValidator.js
@@ -1,12 +1,39 @@
-var FormValidator = (function FormValidator() {
-	var errors = [];
-	var tempError = null;
+/*
 
-	function validateField(data, obj, field) {
+FormValidator by Alberto Sarullo - alberto.sarullo(at)gmail.com
+
+*/
+
+var FormValidator = (function FormValidator() {
+	'use strict';
+
+	var errors = [],
+		tempError = null,
+
+		rulesNumber = 0,
+		asyncFunctionCounter = 0,
+		successCounter = 0,
+		failCounter = 0,
+
+		doneFunction,
+		failFunction,
+		alwaysFunction;
+
+	function isArray(obj) {
+		return Object.prototype.toString.call(obj) === '[object Array]';
+	}
+
+	function isFunction(obj) {
+		return typeof obj === 'function';
+	}
+
+	function validateField(data, obj, field, successCallback, failCallback) {
 		var returnValue;
-		if (typeof obj.rule === 'function') {
+		if (isFunction(obj.rule)) {
+
 			// only 1 parameter: sync version
-			if (obj.rule.length === 1 ) {
+			if (obj.rule.length === 1) {
+				// asyncFunctionCounter++;
 				returnValue = obj.rule(data[field]);
 				if (returnValue === false) {
 					if (tempError === null) {
@@ -14,61 +41,130 @@ var FormValidator = (function FormValidator() {
 							field: field,
 							msg:obj.message || field
 						};
-						errors.push(tempError);
+						failCallback(tempError);
 					}
+				} else {
+					successCallback();
 				}
+				// todo: async with promises return {done:f(){}, fail:f(){}}
+			
+			} else {
+				// async version
+				asyncFunctionCounter++;
+				returnValue = obj.rule(data[field], function successRule() {
+					successCallback();
+				}, function failRule() {
+					//if (tempError === null) {
+						tempError = {
+							field: field,
+							msg:obj.message
+						};
+						failCallback(tempError);
+					//}
+				});
 			}
 		} else {
+			// asyncFunctionCounter++;
 			if (!obj.rule.test(data[field])) {
 				if (tempError === null) {
 					tempError = {
 						field: field,
 						msg:obj.message || field
 					};
-					errors.push(tempError);
+					failCallback(tempError);
 				}
+			} else {
+				successCallback();
 			}
 		}
 	}
 
-	function validateDataWithRules(data, rules) {
+	function countRules(rules) {
+		rulesNumber = 0;
+		asyncFunctionCounter = 0;
+		failCounter = 0;
+		successCounter = 0;
+		var field,
+			rule,
+			i;
+		for (field in rules) {
+			rule = rules[field];
+			if (isArray(rule)) {
+				for (i = 0; i < rule.length; i++) {
+					rulesNumber++;
+				}
+			} else {
+				rulesNumber++;
+			}
+		}
+	}
+
+	function validateDataWithRules(data, rules, successCallback, failCallback) {
 		var i,
 			rule,
 			field;
 		errors = [];
+
+		function success() {
+			successCounter++;
+			console.log(failCounter, successCounter, rulesNumber);
+			if (successCounter === rulesNumber) {
+				successCallback();
+			}
+		}
+
+		function fail(error) {
+			errors.push(error);
+			console.log(error.msg);
+			failCounter++;
+			console.log(failCounter, successCounter, rulesNumber);
+			if (failCounter + successCounter === rulesNumber) {
+				failCallback();
+			}
+		}
+
 		for (field in rules) {
 			rule = rules[field];
-			if (Object.prototype.toString.call(rule) === '[object Array]') {
+			if (isArray(rule)) {
 				tempError = null;
 				for (i = 0; i < rule.length; i++) {
-					validateField(data, rule[i], field);
+					validateField(data, rule[i], field, success, fail);
 				}
 			} else {
 				tempError = null;
-				validateField(data, rule, field);
+				validateField(data, rule, field, success, fail);
 			}
 		}
 	}
 
 	return {
 		validate: function(data, rules) {
-			validateDataWithRules(data, rules);
+			var that = this;
+			setTimeout(function() {
+				countRules(rules);
+				validateDataWithRules(data, rules, function success() {
+					if (isFunction(doneFunction)) {
+						doneFunction.call(that);
+					}
+				}, function fail() {
+					if (isFunction(failFunction)) {
+						failFunction.call(that, errors);
+					}
+				});
+			}, 10);
 			return this;
 		},
 		done: function done(f) {
-			if (errors.length === 0) {
-				f.call(this);
-			}
+			doneFunction = f;
 			return this;
 		},
 		fail: function fail(f) {
-			if (errors.length > 0) {
-				f.call(this, errors);
-			}
+			failFunction = f;
 			return this;
 		},
 		always: function always(f) {
-			f.call(this);
+			alwaysFunction = f;
+			// f.call(this);
 			return this;
 		},
 		EMAIL: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
