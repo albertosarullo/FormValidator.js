06f732c76d6464932381551bc8dbd8bf3311b9c5
Alberto Sarullo <alberto.sarullo@gmail.com>
2014-04-27 17:35:48 +0200

    Updated rules

--- script/main.js
+++ script/main.js
@@ -4,6 +4,10 @@ $(function() {
 		name: {
 			// function rule
 			rule: function(value) {
+				/*setTimeout(function() {
+				//	errorCallback();
+				}, 2000);
+*/
 				return value == 1;
 			},
 			message:"Devi indicare il tuo nome"
@@ -14,42 +18,34 @@ $(function() {
 			message:"Devi indicare il tuo cognome"
 		},
 		email: [{  // cascading rule
-			rule: /^[A-Za-z0-9 ]{10,20}$/,
-			message:"La mail deve essere compresa tra 10 e 20 caratteri"
-		}, {
-			rule: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
-			message:"Devi indicare un indirizzo e-mail valido"
+			rule: /^[A-Za-z0-9@ ]{6,7}$/,
+			message:"La mail deve essere compresa tra 6 e 7 caratteri"
+		}, {  // cascading rule
+			rule: /^[A-Za-z0-9@ ]{7,7}$/,
+			message:"La mail deve essere compresa tra 7 e 7 caratteri"
 		}],
 		
 		comment: {
 			// function rule
-			rule: function(value) {
-				return value == 1;
+			rule: function(value, successCallback, errorCallback) {
+				setTimeout(function() {
+					if (value == 1) {
+						successCallback();
+					} else {
+						errorCallback();
+					}
+				}, 1000);
 			},
 			message:"Devi scrivere un commento"
 		},
 		age: {
 			// function rule
 			rule: function(value) {
-				return value > 18;
+				return value > 1;
 			},
 			message:"Devi indicare la tua età"
 		}
 	};
-
-
-	/* ASYNC
-			comment: {
-			// function rule
-			rule: function(value, successCallback, errorCallback) {
-				setTimeout(function() {
-					successCallback();
-				}, 1000);
-				return value == 1;
-			},
-			message:"Devi scrivere un commento"
-		},
-*/
 	
 	$form.on('submit', function(e) {
 		e.preventDefault();
@@ -63,7 +59,7 @@ $(function() {
 		$('.input-error', $form).removeClass('input-error');
 		
 		
-		formValidator()
+		FormValidator
 			.validate(data, rules)
 			.done(function() {
 				console.log('success');
@@ -71,8 +67,8 @@ $(function() {
 			.fail(function(errors) {
 				console.log('fail');
 				$.each(errors, function(index, error) {
-					// console.log(error.field);
-					var $field = $('#' + error.field, $form);
+					//console.log(error.field);
+					var $field = $("[name='" + error.field + "']", $form);
 					$field.addClass('input-error');
 					$field.parent().children('.error-detail').html(error.msg);
 				});
